2298347910f5836a27c233666ba74ae353a2911d
anandevo13 <100212329+anandevo13@users.noreply.github.com>
2023-09-25 09:04:18 +0530

    Delete 7. Strings directory

--- 7. Strings/12. Recursion/Qi.py
+++
@@ -1,9 +0,0 @@
-x = 75
-
-def myfunc():
-    global x
-    x = x+1
-    print(x)
-    
-myfunc()
-print(x)
--- 7. Strings/9. Set/Q7.py
+++
@@ -1,9 +0,0 @@
-"""
-Q. Python script to check whether a given set is a subset of another given set or not.
-"""
-
-A = {1,2,3}
-B = {1,2,3,4,5}
-
-# all items of A are present in B
-print(A.issubset(B))
